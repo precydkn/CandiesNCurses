613f34ae88d74d904fbf779bc0364f18e36e4428
precydkn <diokno.precy@gmail.com>
2024-05-31 19:12:21 +0400

    index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let bgMusic = document.getElementById('music'),
 audio = new Audio("img/home/cnc main.mp3"), //get audio file
 musicIsPlaying = false, //music is not playing on website load
 bgMusicImage = bgMusic.querySelector('img'); //get img element inside the button
-
+audio.volume = 0.4; //lessen music volume
 //loop music
 audio.addEventListener('ended', function() {
     this.currentTime = 0;
