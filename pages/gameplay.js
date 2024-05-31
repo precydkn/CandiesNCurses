5db0f838a62a421626195bd0820e46f0eecc4f2d
precydkn <diokno.precy@gmail.com>
2024-05-31 19:13:51 +0400

    gameplay.js

--- pages/gameplay.js
+++ pages/gameplay.js
@@ -90,7 +90,7 @@ let bgMusic = document.getElementById('music-g'),
 audio = new Audio("p-img/gameplay.mp3"), //get audio file
 musicIsPlaying = false, //music is not playing on website load
 bgMusicImage = bgMusic.querySelector('img'); //get img element inside the button
-
+audio.volume = 0.4; //lessen music volume
 //loop music
 audio.addEventListener('ended', function() {
     this.currentTime = 0;
